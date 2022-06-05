c7464d88f4235ddeb33260900997f0f4ff8529b5
ShisuiMadara <2020ucs0090@iitjammu.ac.in>
2022-06-05 21:22:36 +0530

    testnet changes

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { App } from "./src/app";
 
-const RPC_URL = "https://hangzhounet.smartpy.io";
+const RPC_URL = "https://ithacanet.smartpy.io/";
 
-new App(RPC_URL).getContractEntrypoints("KT1Gm3XHrRCTJ3TNH6kB3Nt4pURv4XR3kTv8");
+new App(RPC_URL).getBalance("tz1RNPit7S2hg6v9HhSa1jZCwCyGu7kMGTTk");
