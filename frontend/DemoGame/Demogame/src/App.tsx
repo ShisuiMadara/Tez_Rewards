b80e11e61761fa800ea5ceb2bc550b0d018c86e0
TejaswiBishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-06-07 22:03:36 +0530

    Game's frontend changed

--- frontend/DemoGame/Demogame/src/App.tsx
+++ frontend/DemoGame/Demogame/src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DAppProvider } from './dapp/dapp'
+import './index.css';
 
-function App() {
+function ReactComponent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>Hello world!</p>
   );
 }
 
-export default App;
+export default ReactComponent;
